baf35f0c4b8e3bf5b423c14bc9cb8e5152fe2c58
Sara Torrey <sara.moravej@gmail.com>
2020-03-22 15:50:31 +0100

    Add Thymeleaf dependency Add landing controller and html page

---
+++ src/main/java/com/saratorrey/preschool/Controller.java
@@ -0,0 +1,12 @@
+package com.saratorrey.preschool;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+@org.springframework.stereotype.Controller
+public class Controller {
+
+    @GetMapping( value="/" )
+    public String landing() {
+        return "landing";
+    }
+}
